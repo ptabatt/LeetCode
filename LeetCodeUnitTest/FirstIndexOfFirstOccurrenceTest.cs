129f15f7597f15a11affaf44500086f835f938ab
Peter Tabatt <ptabatt@gmail.com>
2023-09-26 14:51:23 -0600

    First index of first occurrence problem + tests

---
+++ LeetCodeUnitTest/FirstIndexOfFirstOccurrenceTest.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class FirstIndexOfFirstOccurrenceTest
+    {
+        [TestCase("sadbutsad", "sad", 0)]
+        [TestCase("leetcode", "leeto", -1)]
+        [TestCase("a", "a", 0)]
+        [TestCase("aaa", "aaaa", -1)]
+        [TestCase("aaaba", "bab", -1)]
+        public void FirstIndexOfFirstOccurence_BasicTest(string haystack, string needle, int expected)
+        {
+            var sut = new FirstIndexOfFirstOccurrenceProblem();
+
+            Assert.AreEqual(expected, sut.StrStr(haystack, needle));
+        }
+    }
+}
