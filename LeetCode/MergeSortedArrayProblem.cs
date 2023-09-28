3042e7af0912c9289c17d5c09c3b6bbda8bcecc7
Peter Tabatt <ptabatt@gmail.com>
2023-09-28 16:44:52 -0600

    I hate this question and I hate my solution even more...

---
+++ LeetCode/MergeSortedArrayProblem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class MergeSortedArrayProblem
+    {
+        //You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+        //and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+        //Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+        //The final sorted array should not be returned by the function, but instead be stored
+        //inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements
+        //denote the elements that should be merged, and the last n elements are set to 0 and should be ignored.
+        //nums2 has a length of n.
+
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            var lst = new List<int>();
+            for (int i = 0; i < m; i++)
+            {   
+                lst.Add(nums1[i]);
+            }
+            foreach (var num in nums2)
+            {
+                lst.Add(num);
+            }
+
+            lst.Sort();
+            var nums3 = lst.ToArray();
+
+            for (int i=0; i <= nums3.Length - 1; i++)
+            {
+                nums1[i] = nums3[i];
+            }
+        }
+    }
+}
---
+++ LeetCodeUnitTest/MergeSortedArrayTest.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class MergeSortedArrayTest
+    {
+        [Test]
+        public void PassByReference_Test()
+        {
+            var sut = new MergeSortedArrayProblem();
+            int[] arr1 = new[] { 1, 2, 3, 0, 0, 0 };
+            int[] arr2 = new[] { 1, 2, 3 };
+
+            sut.Merge(arr1, 6, arr2, 3);
+
+            Assert.AreEqual(3, arr1[5]);
+        }
+
+        [TestCase(new[] { 1, 2, 3, 0, 0, 0 }, 3, new[] { 2, 5, 6 }, 3, new[] { 1, 2, 2, 3, 5, 6 })]
+        [TestCase(new[] { 1 }, 1, new int[] { }, 0, new[] { 1 })]
+        [TestCase(new int[] { 0 }, 0, new [] { 1 }, 1, new[] { 1 })]
+        [TestCase(new int[] { -1, 0, 0, 3, 3, 3, 0, 0, 0 }, 6, new[] { 1, 2, 2 }, 3, new[] { -1, 0, 0, 1, 2, 2, 3, 3, 3 })]
+        [TestCase(new[] { 0, 0, 3, 0, 0, 0, 0, 0, 0 }, 3, new int[] { -1, 1, 1, 1, 2, 3 }, 6, new[] { -1, 0, 0, 1, 1, 1, 2, 3, 3 })]
+        public void MergeSortedArray_BasicTests(int[] nums1, int m, int[] nums2, int n, int[] expected)
+        {
+            var sut = new MergeSortedArrayProblem();
+            sut.Merge(nums1, m, nums2, n);
+
+            Assert.AreEqual(expected, nums1);
+        }
+    }
+}
