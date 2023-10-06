6066e4abb8d8eb7dd81e647240bdde11150b4a38
Peter Tabatt <ptabatt@gmail.com>
2023-10-05 20:19:46 -0600

    revert to simpler solution

--- LeetCode/MergeSortedArrayProblem.cs
+++ LeetCode/MergeSortedArrayProblem.cs
@@ -16,24 +16,12 @@ namespace LeetCode
 
         public void Merge(int[] nums1, int m, int[] nums2, int n)
         {
-            int r1 = m - 1;
-            int r2 = n - 1;
-
-            for (int w = m + n-1; w >= 0; w--)
+            for (int i = 0; i < n; i++)
             {
-                if(r1 >= 0 && r2 >= 0)
-                {
-                    nums1[w] = nums1[r1] > nums2[r2] ? nums1[r1--] : nums2[r2--];
-                } 
-                else if (r1 >= 0)
-                {
-                    nums1[w] = nums1[r1--];
-                }
-                else
-                {
-                    nums1[w] = nums2[r2--];
-                }
+                nums1[m + i] = nums2[i];
             }
+
+            Array.Sort(nums1);
         }
     }
 }
