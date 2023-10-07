e3c1e16ff0b8593e49cd4d4dd4747bdd6cfbfdf9
Peter Tabatt <ptabatt@gmail.com>
2023-10-06 20:23:55 -0600

    RemoveElement problem

---
+++ LeetCode/RemoveElementProblem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class RemoveElementProblem
+    {
+        //Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+        //The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+        //Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+        //Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
+        //The remaining elements of nums are not important as well as the size of nums.
+        //Return k.
+
+        public int RemoveElement(int[] nums, int val)
+        {
+            int reader = 0, writer = 0;
+
+            while (reader < nums.Length)
+            {
+                if (nums[reader] == val)
+                {
+                    reader++;
+                }
+                else
+                {
+                    nums[writer] = nums[reader];
+                    reader++;
+                    writer++;
+                }
+            }
+
+            return writer;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/RemoveElementTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using LeetCode;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class RemoveElementTest
+    {
+        [Test]
+        public void RemoveElement_BasicTest()
+        {
+            var nums = new[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+            var val = 2;
+
+            var objSut = new RemoveElementProblem();
+            var result = objSut.RemoveElement(nums, val);
+
+            var numSlice = nums.Skip(0).Take(result);
+
+            Assert.AreEqual(5, result);
+            Assert.That(numSlice, Has.Exactly(2).EqualTo(0));
+            Assert.That(numSlice, Has.Exactly(1).EqualTo(1));
+            Assert.That(numSlice, Has.Exactly(1).EqualTo(4));
+            Assert.That(numSlice, Has.Exactly(1).EqualTo(3));
+        }
+    }
+}
