06bf22e681fa4146fc4758e2f29212cb3ac79aea
Peter Tabatt <ptabatt@gmail.com>
2023-10-08 19:51:08 -0600

    ProductExceptItself

---
+++ LeetCode/ProductExeceptItselfProblem.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class ProductExeceptItselfProblem
+    {
+        //Given an integer array nums, return an array answer such that answer[i] is equal to
+        //the product of all the elements of nums except nums[i].
+
+        //The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+        //You must write an algorithm that runs in O(n) time and without using the division operation.
+
+        //Constraints:
+        //2 <= nums.length <= 105
+        //-30 <= nums[i] <= 30
+        //The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+        public int[] ProductExceptSelf_Naive(int[] nums)
+        {
+            int?[] arrOfNulls = new int?[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    else if (arrOfNulls[i] == null)
+                    {
+                        arrOfNulls[i] = nums[j];
+                    }
+                    else
+                    {
+                        arrOfNulls[i] *= nums[j];
+                    }
+                }
+            }
+
+            int[] result = new int[arrOfNulls.Length];
+            for (int i = 0; i < arrOfNulls.Length; i++)
+            {
+                result[i] = arrOfNulls[i] == null ? 0 : arrOfNulls[i].Value;
+            }
+
+            return result;
+        }
+
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            var prefProd = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i == 0)
+                {
+                    prefProd[i] = 1;
+                } 
+                else
+                {
+                    prefProd[i] = nums[i - 1] * prefProd[i - 1];
+                }
+            }
+
+            var suffProd = new int[nums.Length];
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (i == nums.Length - 1)
+                {
+                    suffProd[i] = 1;
+                } 
+                else
+                {
+                    suffProd[i] = nums[i + 1] * suffProd[i + 1];
+                }
+            }
+
+            var result = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result[i] = prefProd[i] * suffProd[i];
+            }
+
+            return result;
+        }   
+    }
+}
---
+++ LeetCodeUnitTest/ProductExceptItselfTest.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class ProductExceptItselfTest
+    {
+        [TestCase(new[] { 1, 2, 3, 4 }, new[] { 24, 12, 8, 6 })]
+        [TestCase(new[] { -1, 1, 0, -3, 3 }, new[] { 0, 0, 9, 0, 0 })]
+        public void ProductExeceptIteselfTest_Naive(int[] input, int[] expected)
+        {
+            var sut = new ProductExeceptItselfProblem();
+
+            var actual = sut.ProductExceptSelf_Naive(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new[] { 1, 2, 3, 4 }, new[] { 24, 12, 8, 6 })]
+        [TestCase(new[] { -1, 1, 0, -3, 3 }, new[] { 0, 0, 9, 0, 0 })]
+        public void ProductExeceptIteselfTest(int[] input, int[] expected)
+        {
+            var sut = new ProductExeceptItselfProblem();
+
+            var actual = sut.ProductExceptSelf(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
