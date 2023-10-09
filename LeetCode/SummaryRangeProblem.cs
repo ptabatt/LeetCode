7f713d717e3f0d6f505f1f22318e7452063aea4c
Peter Tabatt <ptabatt@gmail.com>
2023-10-09 16:08:22 -0600

    Summary Range Problem

---
+++ LeetCode/SummaryRangeProblem.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class SummaryRangeProblem
+    {
+        //You are given a sorted unique integer array nums.
+
+        //A range[a, b] is the set of all integers from a to b(inclusive).
+
+        //Return the smallest sorted list of ranges that cover all the numbers in the array exactly.
+        //That is, each element of nums is covered by exactly one of the ranges, and there 
+        //is no integer x such that x is in one of the ranges but not in nums.
+
+        //Each range[a, b] in the list should be output as:
+
+        //"a->b" if a != b
+        //"a" if a == b
+        public IList<string> SummaryRanges(int[] nums)
+        {
+            var result =  new List<string>();
+
+            if (nums.Length == 0)
+            {
+                return result;
+            }
+
+            var sequenceBegin = nums[0];
+            var sequenceEnd = nums[0];
+
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                if (i == nums.Length)
+                {
+                    if (sequenceBegin == sequenceEnd)
+                    {
+                        result.Add($"{sequenceBegin}");
+                    }
+                    else
+                    {
+                        result.Add($"{sequenceBegin}->{sequenceEnd}");
+                    }
+                }
+                else if (nums[i] - 1 == sequenceEnd)
+                {
+                    sequenceEnd = nums[i];
+                }
+                else
+                {
+                    if (sequenceBegin == sequenceEnd)
+                    {
+                        result.Add($"{sequenceBegin}");
+                    }
+                    else
+                    {
+                        result.Add($"{sequenceBegin}->{sequenceEnd}");
+                    }
+
+                    sequenceBegin = nums[i];
+                    sequenceEnd = nums[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/SummaryRangeTest.cs
@@ -0,0 +1,66 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class SummaryRangeTest
+    {
+        [Test]
+        public void SummaryRangeTest_Example1()
+        {
+            var sut = new SummaryRangeProblem();
+
+            var input = new[] { 0, 1, 2, 4, 5, 7 };
+
+            var actual = sut.SummaryRanges(input);
+
+            Assert.That(actual.Count == 3, $"Expected Count of 3. Got Count {actual.Count}");
+            Assert.That(actual.Contains("0->2"), "0->2 is missing from the list");
+            Assert.That(actual.Contains("4->5"), "4->5 is missing from the list");
+            Assert.That(actual.Contains("7"), "7 is missing from the list");
+        }
+
+        [Test]
+        public void SummaryRangeTest_Example2()
+        {
+            var sut = new SummaryRangeProblem();
+
+            var input = new[] { 0, 2, 3, 4, 6, 8, 9 };
+
+            var actual = sut.SummaryRanges(input);
+
+            Assert.That(actual.Count == 4, $"Expected Count of 4. Got Count {actual.Count}");
+            Assert.That(actual.Contains("0"), "0 is missing from the list");
+            Assert.That(actual.Contains("2->4"), "2->4 is missing from the list");
+            Assert.That(actual.Contains("6"), "6 is missing from the list");
+            Assert.That(actual.Contains("8->9"), "8->9 is missing from the list");
+        }
+
+        [Test]
+        public void SummaryRangeTest_Empty()
+        {
+            var sut = new SummaryRangeProblem();
+
+            var input = new int[] { };
+
+            var actual = sut.SummaryRanges(input);
+
+            Assert.That(actual.Count == 0, $"Expected Count of 0. Got Count {actual.Count}");
+        }
+
+        [Test]
+        public void SummaryRangeTest_SingleElement()
+        {
+            var sut = new SummaryRangeProblem();
+
+            var input = new int[] { 100 };
+
+            var actual = sut.SummaryRanges(input);
+
+            Assert.That(actual.Count == 1, $"Expected Count of 0. Got Count {actual.Count}");
+
+            Assert.That(actual.Contains("100"), "100 is missing from the list");
+        }
+    }
+}
