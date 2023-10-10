e19ae07b7f774682dc92c0c07e9f727fc38585fa
Peter Tabatt <ptabatt@gmail.com>
2023-10-10 11:57:15 -0600

    Ransome note problem (super easy)

---
+++ LeetCode/RandomNoteProblem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class RandomNoteProblem
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            var dctNote = new Dictionary<char, int>();
+            var dctMag = new Dictionary<char, int>();
+
+            foreach (var c in ransomNote)
+            {
+                if (dctNote.ContainsKey(c))
+                {
+                    dctNote[c] += 1;
+                }
+                else
+                {
+                    dctNote[c] = 1;
+                }
+            }
+
+            foreach (var c in magazine)
+            {
+                if (dctMag.ContainsKey(c))
+                {
+                    dctMag[c] += 1;
+                }
+                else
+                {
+                    dctMag[c] = 1;
+                }
+            }
+
+            foreach (var entry in dctNote)
+            {
+                int noteVal; 
+                if (!dctMag.TryGetValue(entry.Key, out noteVal))
+                {
+                    return false;
+                }
+
+                if(entry.Value > noteVal)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/RansomNoteTest.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class RansomNoteTest
+    {
+        [TestCase("a", "b", false)]
+        [TestCase("aa", "ab", false)]
+        [TestCase("aa", "aab", true)]
+        public void RansomeNote_BasicTests(string ransomNote, string magazine, bool expected)
+        {
+            var objSut = new RandomNoteProblem();
+
+            Assert.AreEqual(expected, objSut.CanConstruct(ransomNote, magazine));
+        }
+    }
+}
