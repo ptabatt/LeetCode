e2bb6ecfbc4f2af131a19e083431417058d36eea
Peter Tabatt <ptabatt@gmail.com>
2023-10-09 20:25:23 -0600

    LRU cache impl + test!

---
+++ LeetCode/LRUCache.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class LRUListNode
+    {
+        public int Key;
+        public int Val;
+        public LRUListNode Next { get; set; }
+        public LRUListNode Prev { get; set; }
+
+        public LRUListNode(int key, int val)
+        {
+            Key = key;
+            Val = val;
+        }
+    }
+    public class LRUCache
+    {
+        private int _capacity;
+        private Dictionary<int, LRUListNode> _dic;
+        private LRUListNode _head;
+        private LRUListNode _tail; 
+
+        public LRUCache(int capacity)
+        {
+            _capacity = capacity;
+            _dic = new Dictionary<int, LRUListNode>();
+            _head = new LRUListNode(-1, -1);
+            _tail = new LRUListNode(-1, -1);
+            _head.Next = _tail;
+            _tail.Prev = _head;
+        }
+
+        public int Get(int key)
+        {
+            if (_dic.TryGetValue(key, out var node))
+            {
+                Remove(node);
+                Add(node);
+                return node.Val;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+
+        public void Put(int key, int value)
+        {
+            if (_dic.ContainsKey(key))
+            {
+                var oldNode = _dic[key];
+                Remove(oldNode);
+            }
+
+            var node = new LRUListNode(key, value);
+            _dic[key] = node;
+            Add(node);
+
+            if (_dic.Count > _capacity)
+            {
+                var nodeToDelete = _head.Next;
+                Remove(nodeToDelete);
+                _dic.Remove(nodeToDelete.Key);
+            }
+        }
+
+        private void Add(LRUListNode node)
+        {
+            var previousEnd = _tail.Prev;
+            previousEnd.Next = node;
+            node.Prev = previousEnd;
+            node.Next = _tail;
+            _tail.Prev = node;
+        }
+
+        private void Remove(LRUListNode node)
+        {
+            node.Prev.Next = node.Next;
+            node.Next.Prev = node.Prev;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/LRUCacheTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using LeetCode;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class LRUCacheTest
+    {
+        [Test]
+        public void LRUCache_ExampleTest()
+        {
+            LRUCache lRUCache = new LRUCache(2);
+            lRUCache.Put(1, 1); // cache is {1=1}
+            lRUCache.Put(2, 2); // cache is {1=1, 2=2}
+            Assert.AreEqual(1, lRUCache.Get(1));    // return 1
+            lRUCache.Put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+            Assert.AreEqual(-1, lRUCache.Get(2));    // returns -1 (not found)
+            lRUCache.Put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+            Assert.AreEqual(-1, lRUCache.Get(1));    // return -1 (not found)
+            Assert.AreEqual(3, lRUCache.Get(3));    // return 3
+            Assert.AreEqual(4, lRUCache.Get(4));    // return 4
+        }
+    }
+}
