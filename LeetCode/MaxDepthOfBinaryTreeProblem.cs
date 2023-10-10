13bfcb109bb8a9c590663c3fe433c24be114d792
Peter Tabatt <ptabatt@gmail.com>
2023-10-09 18:35:05 -0600

    MaxDepthOfBinaryTreeProblem

---
+++ LeetCode/MaxDepthOfBinaryTreeProblem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class MaxDepthOfBinaryTreeProblem
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            int maxRight, maxLeft;
+            if (root.left != null)
+            {
+                maxRight = MaxDepth(root.left) + 1;
+            }
+            else
+            {
+                maxRight = 0;
+            }
+
+            if (root.right != null)
+            {
+                maxLeft = MaxDepth(root.right) + 1;
+            }
+            else
+            {
+                maxLeft = 0;
+            }
+
+            return Math.Max(maxRight, Math.Max(maxLeft, 1));
+        }
+    }
+}
---
+++ LeetCodeUnitTest/MaxDepthOfBinaryTreeTest.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using LeetCode;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class MaxDepthOfBinaryTreeTest
+    {
+        [Test]
+        public void MaxDepthOfBinaryTree_BasicExample()
+        {
+            var objSut = new MaxDepthOfBinaryTreeProblem();
+
+            var root = new TreeNode
+            {
+                val = 3,
+                left = new TreeNode
+                {
+                    val = 9
+                },
+                right = new TreeNode
+                {
+                    val = 20,
+                    left = new TreeNode
+                    {
+                        val = 15
+                    },
+                    right = new TreeNode
+                    {
+                        val = 7
+                    }
+                }
+            };
+
+            Assert.AreEqual(3, objSut.MaxDepth(root));
+        }
+
+        [Test]
+        public void MaxDepthOfBinaryTree_SingleNode()
+        {
+            var objSut = new MaxDepthOfBinaryTreeProblem();
+
+            var root = new TreeNode
+            {
+                val = 5,
+            };
+
+            Assert.AreEqual(1, objSut.MaxDepth(root));
+        }
+
+        [Test]
+        public void MaxDepthOfBinaryTree_TwoNodes()
+        {
+            var objSut = new MaxDepthOfBinaryTreeProblem();
+
+            var root = new TreeNode
+            {
+                val = 5,
+                right = new TreeNode
+                {
+                    val = 7
+                }
+            };
+
+            Assert.AreEqual(2, objSut.MaxDepth(root));
+        }
+
+        [Test]
+        public void MaxDepthOfBinaryTree_NullRoot()
+        {
+            var objSut = new MaxDepthOfBinaryTreeProblem();
+
+            Assert.AreEqual(0, objSut.MaxDepth(null));
+        }
+    }
+}
