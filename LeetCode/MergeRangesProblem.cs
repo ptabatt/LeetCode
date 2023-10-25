8f91711b94a478ccde56ad1e6c67b8530e938696
Peter Tabatt <ptabatt@gmail.com>
2023-10-25 16:56:40 -0600

    Stupid HackerRank problem

---
+++ LeetCode/MergeRangesProblem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class MergeRangesProblem
+    {
+        // A stupid HackerRank problem I had during my Oracle interview.
+        // Basically, given a 2D Array each with an lower-bound and upper-bound value
+        // "Merge" the two arrays into one.
+        // i.e. [ [1,3], [2, 4] ] => [ [1, 4] ]
+        // [ [1,3], [6, 7] ] => [ [1,3], [6, 7] ] (remains unchanged)
+        public int[][] MergeRanges(int[][] ranges)
+        {
+            if (ranges == null || ranges.Length == 0)
+            {
+                return null;
+            }
+
+            Array.Sort(ranges, (a, b) => { return a[0] - b[0]; });
+
+            var result = new List<int[]>();
+            result.Add(ranges[0]);
+
+            for (int i = 1; i < ranges.Length; i++)
+            {
+                if (ranges[i] == null || ranges[i].Length != 2)
+                    return null;
+
+                var last = result.Last();
+                
+                if (last[1] >= ranges[i][0])
+                {
+                    last[1] = ranges[i][1];
+                }
+                else
+                {
+                    result.Add(ranges[i]);
+                }
+            }
+
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ LeetCodeUnitTest/MergeRangesTest.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class MergeRangesTest
+    {
+        [Test]
+        public void MergeRangesBasicTests()
+        {
+            var objSut = new MergeRangesProblem();
+
+            var positive = new int[][] { new int[] { 1, 3 }, new int[] { 2, 4 }, new int[] { 7, 9 } };
+            var expected = new int[][] { new int[] { 1, 4 }, new int[] { 7, 9 } };
+
+            Assert.AreEqual(expected, objSut.MergeRanges(positive));
+
+            var negative = new int[][] { new int[] { 1, 2 }, new int[] { 3, 4 } };
+
+            Assert.AreEqual(negative, objSut.MergeRanges(negative));
+
+            var unOrdered = new int[][] { new int[] { 2, 4 }, new int[] { 1, 3 }, new int[] { 7, 9 } };
+
+            Assert.AreEqual(expected, objSut.MergeRanges(unOrdered));
+
+            var boundary = new int[][] { new int[] { 1, 3 }, new int[] { 3, 4 }, new int[] { 7, 9 } };
+
+            Assert.AreEqual(expected, objSut.MergeRanges(boundary));
+        }
+    }
+}
