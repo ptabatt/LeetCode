d22d8e3fe5f466b139d8cb00fa1acc24704e1d53
Peter Tabatt <ptabatt@gmail.com>
2023-11-01 12:56:50 -0600

    Finally finished the RotateImage problem

--- LeetCode/RotateImageProblem.cs
+++ LeetCode/RotateImageProblem.cs
@@ -1,11 +1,129 @@
-﻿namespace LeetCode
+﻿using System;
+
+namespace LeetCode
 {
     public class RotateImageProblem
     {
-        //https://leetcode.com/problems/rotate-image/?envType=study-plan-v2&envId=top-interview-150
+        // https://leetcode.com/problems/rotate-image/?envType=study-plan-v2&envId=top-interview-150
         public void Rotate(int[][] matrix)
         {
-            // Gonna think on this one for a bit...
+            var n = matrix.Length;
+            if (n <= 1)
+            {
+                return;
+            }
+
+            // Create algorithm to traverse matrix border and rotate columns/rows
+            // Run the algorithm m times where m = the floor of n / 2 (we're using an n x n grid here)
+
+            for (var i = 0; i < Math.Floor(new decimal(n/2)); i++)
+            {
+                var firstIdx = i;
+                var lastIdx = n - 1 - i;
+
+                var tmpArrIdx = 0;
+
+                // put top row in right column
+
+                var tmpArr = CreateTmpArrFromSubMatrix(matrix[i], i);
+
+                Print2DArrayAndTmpArray(matrix, tmpArr);
+
+                for (var currentRowIdx = firstIdx; currentRowIdx <= lastIdx; currentRowIdx++)
+                {
+                    Swap(matrix, currentRowIdx, lastIdx, tmpArr, tmpArrIdx);
+                    tmpArrIdx++;
+                }
+
+                tmpArrIdx = 0;
+
+                Print2DArrayAndTmpArray(matrix, tmpArr);
+
+                // put left column in bottom row (but in reverse)
+
+                Array.Reverse(tmpArr);
+
+                for (var currentColIdx = firstIdx; currentColIdx <= lastIdx; currentColIdx++)
+                {
+                    Swap(matrix, lastIdx, currentColIdx, tmpArr, tmpArrIdx);
+                    tmpArrIdx++;
+                }
+
+                tmpArrIdx = 1;
+
+                Print2DArrayAndTmpArray(matrix, tmpArr);
+
+                // put bottom row in left column (but in reverse)
+
+                Array.Reverse(tmpArr);
+
+                for (var currentRowIdx = lastIdx - 1; currentRowIdx >= firstIdx; currentRowIdx--)
+                {
+                    Swap(matrix, currentRowIdx, firstIdx, tmpArr, tmpArrIdx);
+                    tmpArrIdx++;
+                }
+
+                tmpArrIdx = 1;
+
+                Print2DArrayAndTmpArray(matrix, tmpArr);
+
+                // put right column in top row
+
+                Array.Reverse(tmpArr);
+
+                for (var currentColIdx = lastIdx - 1; currentColIdx > firstIdx; currentColIdx--)
+                {
+                    Swap(matrix, firstIdx, currentColIdx, tmpArr, tmpArrIdx);
+                    tmpArrIdx++;
+                }
+
+                Print2DArrayAndTmpArray(matrix, tmpArr);
+            }
+        }
+
+        void Print2DArrayAndTmpArray(int[][] matrix, int[] arr)
+        {
+            System.Diagnostics.Debug.WriteLine("**********MATRIX START*************");
+            
+            for (var i = 0; i < matrix.Length; i++)
+            {
+                for (var j = 0; j < matrix.Length; j++)
+                {
+                    System.Diagnostics.Debug.Write(matrix[i][j] + ", ");
+                }
+                System.Diagnostics.Debug.WriteLine("");
+            }
+            
+            System.Diagnostics.Debug.WriteLine("**********MATRIX END***************");
+
+            System.Diagnostics.Debug.WriteLine("**********TMPARR START*************");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                System.Diagnostics.Debug.Write(arr[i] + ", ");
+            }
+
+            System.Diagnostics.Debug.WriteLine("**********TMPARR END***************");
+        }
+
+        int[] CreateTmpArrFromSubMatrix(int[] subMatrix, int offset)
+        {
+            var offSetTimes2 = offset * 2;
+            var tmpArr = new int[subMatrix.Length - offSetTimes2];
+
+            for (var i = 0; i < subMatrix.Length - offSetTimes2; i++)
+            {
+                tmpArr[i] = subMatrix[i + offset];
+            }
+
+            return tmpArr;
+        }
+
+        void Swap(int[][] matrix, int rowIdx1, int colIdx1, int[] arr, int arrIdx)
+        {
+            var tmp = matrix[rowIdx1][colIdx1];
+            matrix[rowIdx1][colIdx1] = arr[arrIdx];
+            arr[arrIdx] = tmp;
         }
     }
 }
--- LeetCodeUnitTest/RotateImageTest.cs
+++ LeetCodeUnitTest/RotateImageTest.cs
@@ -9,6 +9,54 @@ namespace LeetCodeUnitTest
         [Test]
         public void RotateImageBasicTest1()
         {
+            // Extremely basic test to aid debugging/visualization of the problem
+
+            var objSut = new RotateImageProblem();
+
+            var matrixInput = new int[][]
+            {
+                new[] { 1, 2 },
+                new[] { 3, 4 }
+            };
+
+            var matrixExpectedOutput = new int[][]
+            {
+                new[] { 3, 1 },
+                new[] { 4, 2 }
+            };
+
+            objSut.Rotate(matrixInput);
+            Assert.AreEqual(matrixExpectedOutput, matrixInput);
+        }
+
+        [Test]
+        public void RotateImageBasicTest2()
+        {
+            var objSut = new RotateImageProblem();
+
+            var matrixInput = new int[][]
+            {
+                new[] { 1, 2, 3, 4 },
+                new[] { 5, 6, 7, 8 },
+                new[] { 9, 10, 11, 12 },
+                new[] { 13, 14, 15, 16 },
+            };
+
+            var matrixExpectedOutput = new int[][]
+            {
+                new[] { 13, 9, 5, 1 },
+                new[] { 14, 10, 6, 2 },
+                new[] { 15, 11, 7, 3 },
+                new[] { 16, 12, 8, 4 },
+            };
+
+            objSut.Rotate(matrixInput);
+            Assert.AreEqual(matrixExpectedOutput, matrixInput);
+        }
+
+        [Test]
+        public void RotateImageBasicTest_LeetCodeExample1()
+        {
             var objSut = new RotateImageProblem();
 
             var matrixInput = new int[][] 
@@ -30,23 +78,23 @@ namespace LeetCodeUnitTest
         }
 
         [Test]
-        public void RotateImageBasicTest2()
+        public void RotateImageBasicTest_LeetCodeExample2()
         {
             var objSut = new RotateImageProblem();
 
             var matrixInput = new int[][]
             {
-                new[] {5,1,9,11 }, 
-                new[] {2, 4, 8, 10 }, 
-                new[] {13, 3, 6, 7 }, 
-                new[] {15, 14, 12, 16 }
+                new[] { 5, 1, 9, 11 }, 
+                new[] { 2, 4, 8, 10 }, 
+                new[] { 13, 3, 6, 7 }, 
+                new[] { 15, 14, 12, 16 }
             };
 
             var matrixExpectedOutput = new int[][]
             {
-                new[] {15,13,2,5 },
-                new[] {14,3,4,1 },
-                new[] {12,6,8,9 },
+                new[] { 15, 13, 2, 5 },
+                new[] { 14, 3, 4, 1 },
+                new[] { 12, 6, 8, 9 },
                 new[] { 16, 7, 10, 11 }
             };
 
