bf0a474eafe8d6e045d88af3ced8508512033241
Peter Tabatt <ptabatt@gmail.com>
2023-11-02 11:17:25 -0600

    Reverse Polish Notation

---
+++ LeetCode/EvaluateReversePolishNotationProblem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    // https://leetcode.com/problems/evaluate-reverse-polish-notation/?envType=study-plan-v2&envId=top-interview-150
+    public class EvaluateReversePolishNotationProblem
+    {
+        public int EvalRPN(string[] tokens)
+        {
+            var numStack = new Stack<int>();
+
+            foreach (var s in tokens)
+            {
+                if (s == "+" || s == "-" || s == "/" || s == "*")
+                {
+                    numStack.Push(Eval(s, numStack.Pop(), numStack.Pop()));
+                }
+                else
+                {
+                    numStack.Push(Convert.ToInt32(s));
+                }
+            }
+
+            return numStack.Pop(); 
+        }
+
+        int Eval(string e, int a, int b)
+        {
+            switch (e)
+            {
+                case "+":
+                    return b + a;
+                case "-":
+                    return b - a;
+                case "*":
+                    return b * a;
+                case "/":
+                default:
+                    return b / a;
+
+            }
+        }
+    }
+}
---
+++ LeetCodeUnitTest/EvaluateReversePolishNotationTest.cs
@@ -0,0 +1,54 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class EvaluateReversePolishNotationTest
+    {
+        [Test]
+        public void EvaluateReversePolishNotationTest1()
+        {
+            var sut = new EvaluateReversePolishNotationProblem();
+
+            var tokens = new[] { "2", "1", "+", "3", "*" };
+            Assert.AreEqual(9, sut.EvalRPN(tokens));
+        }
+
+        [Test]
+        public void EvaluateReversePolishNotationTest2()
+        {
+            var sut = new EvaluateReversePolishNotationProblem();
+
+            var tokens = new[] { "4", "13", "5", "/", "+" };
+            Assert.AreEqual(6, sut.EvalRPN(tokens));
+        }
+
+        [Test]
+        public void EvaluateReversePolishNotationTest3()
+        {
+            var sut = new EvaluateReversePolishNotationProblem();
+
+            var tokens = new[] { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" };
+            Assert.AreEqual(22, sut.EvalRPN(tokens));
+        }
+
+        [Test]
+        public void EvaluateReversePolishNotationEdgeCase1()
+        {
+            var sut = new EvaluateReversePolishNotationProblem();
+
+            var tokens = new[] { "10" };
+            Assert.AreEqual(10, sut.EvalRPN(tokens));
+        }
+
+        [Test]
+        public void EvaluateReversePolishNotationEdgeCase2()
+        {
+            var sut = new EvaluateReversePolishNotationProblem();
+
+            var tokens = new[] { "3", "11", "+", "5", "-" };
+            Assert.AreEqual(9, sut.EvalRPN(tokens));
+        }
+    }
+}
