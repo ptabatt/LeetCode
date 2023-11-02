ccf0a35be7ca2a5718fc416c6458f8a87c7ac227
Peter Tabatt <ptabatt@gmail.com>
2023-11-02 15:18:39 -0600

    Spiral Order Leetcode problem

---
+++ LeetCode/SpiralMatrixProblem.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class SpiralMatrixProblem
+    {
+        public enum Direction { Right, Down, Left, Up }
+        public IList<int> SpiralOrder(int[][] matrix)
+        {
+            var col = new List<int>();
+            var m = matrix[0].Length;
+            var n = matrix.Length;
+            var cellCount =  m * n;
+            var d = Direction.Right;
+            var offSet = 0;
+            int i = 0, x = 0, y = 0;            
+
+            while (i < cellCount)
+            {
+                col.Add(matrix[y][x]);
+                i++;
+
+                switch (d)
+                {
+                    case Direction.Right:
+                        if (x < (m - 1 - offSet))
+                        {
+                            x++;
+                        }
+                        else
+                        {
+                            y++;
+                            d = Direction.Down;
+                        }
+                        break;
+                    case Direction.Down:
+                        if (y < (n - 1 - offSet))
+                        {
+                            y++;
+                        }
+                        else
+                        {
+                            x--;
+                            d = Direction.Left;
+                        }
+                        break;
+                    case Direction.Left:
+                        if (x > offSet)
+                        {
+                            x--;
+                        }
+                        else
+                        {
+                            y--;
+                            d = Direction.Up;
+                        }
+                        break;
+                    case Direction.Up:
+                        if (y > offSet + 1)
+                        {
+                            y--;
+                        }
+                        else
+                        {
+                            x++;
+                            d = Direction.Right;
+                            // IMPORTANT: the height and width of the rectangle shrinks
+                            // when it's time to go to the right again.
+                            offSet++;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return col;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/SpiralMatrixTest.cs
@@ -0,0 +1,84 @@
+﻿using LeetCode;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class SpiralMatrixTest
+    {
+        [Test]
+        public void SpiralMatrixTestLeetCodeCase1()
+        {
+            var sut = new SpiralMatrixProblem();
+            var given = new int[][] 
+            { 
+                new int[] { 1,2,3 }, 
+                new int[] { 4,5,6 }, 
+                new int[] { 7,8,9 } 
+            };
+            var expected = new List<int> { 1, 2, 3, 6, 9, 8, 7, 4, 5 };
+
+            Assert.AreEqual(expected, sut.SpiralOrder(given));
+        }
+
+        [Test]
+        public void SpiralMatrixTestLeetCodeCase2()
+        {
+            var sut = new SpiralMatrixProblem();
+            var given = new int[][]
+            {
+                new int[] { 1,2,3,4 },
+                new int[] { 5,6,7,8 },
+                new int[] { 9,10,11,12 }
+            };
+            var expected = new List<int> { 1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7 };
+
+            Assert.AreEqual(expected, sut.SpiralOrder(given));
+        }
+
+        [Test]
+        public void SpiralMatrixTestEdgeCase1()
+        {
+            // per instructions, we can't have a null or empty array as input, so
+            // we won't be testing for that here
+            var sut = new SpiralMatrixProblem();
+            var given = new int[][]
+            {
+                new int[] { 100 }
+            };
+            var expected = new List<int> { 100 };
+
+            Assert.AreEqual(expected, sut.SpiralOrder(given));
+        }
+
+        [Test]
+        public void SpiralMatrixTestEdgeCase2()
+        {
+            var sut = new SpiralMatrixProblem();
+            var given = new int[][]
+            {
+                new int[] { 100, 200, 100, 400 }
+            };
+            var expected = new List<int> { 100, 200, 100, 400 };
+
+            Assert.AreEqual(expected, sut.SpiralOrder(given));
+        }
+
+        [Test]
+        public void SpiralMatrixTestEdgeCase3()
+        {
+            var sut = new SpiralMatrixProblem();
+            var given = new int[][]
+            {
+                new int[] { 100 },
+                new int[] { 200 },
+                new int[] { 100 },
+                new int[] { 400 },
+            };
+            var expected = new List<int> { 100, 200, 100, 400 };
+
+            Assert.AreEqual(expected, sut.SpiralOrder(given));
+        }
+    }
+}
