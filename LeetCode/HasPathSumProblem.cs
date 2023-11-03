7bfb914c74c3b34f9fbd5c1eaec6e6ba95d09eab
Peter Tabatt <ptabatt@gmail.com>
2023-11-03 13:10:19 -0600

    HasPathSumProblem

---
+++ LeetCode/HasPathSumProblem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode
+{
+    // https://leetcode.com/problems/path-sum/description/?envType=study-plan-v2&envId=top-interview-150
+    public class HasPathSumProblem
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            if (root.left == null && root.right == null)
+            {
+                return root.val == targetSum;
+            }
+
+            return (root.left == null ? false : HasPathSum(root.left, targetSum - root.val))
+                || (root.right == null ? false : HasPathSum(root.right, targetSum - root.val));
+        }
+    }
+}
---
+++ LeetCodeUnitTest/HasPathSumTest.cs
@@ -0,0 +1,100 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class HasPathSumTest
+    {
+        [Test]
+        public void HasPathSumLeetCodeExample1()
+        {
+            var sut = new HasPathSumProblem();
+
+            var root = new TreeNode
+            {
+                val = 5,
+                left = new TreeNode
+                {
+                    val = 4,
+                    left = new TreeNode
+                    {
+                        val = 11,
+                        left = new TreeNode
+                        {
+                            val = 7
+                        },
+                        right = new TreeNode
+                        {
+                            val = 2
+                        }
+                    }
+                },
+                right = new TreeNode
+                {
+                    val = 8,
+                    left = new TreeNode
+                    {
+                        val = 13
+                    },
+                    right = new TreeNode
+                    {
+                        val = 4,
+                        right = new TreeNode
+                        {
+                            val = 1
+                        }
+                    }
+                }
+            };
+
+            Assert.IsTrue(sut.HasPathSum(root, 22));
+        }
+
+        [Test]
+        public void HasPathSumLeetCodeExample2()
+        {
+            var sut = new HasPathSumProblem();
+
+            var root = new TreeNode
+            {
+                val = 1,
+                left = new TreeNode
+                {
+                    val = 2
+                },
+                right = new TreeNode
+                {
+                    val = 3
+                }
+            };
+
+            Assert.IsFalse(sut.HasPathSum(root, 5));
+        }
+
+        [Test]
+        public void HasPathSumSimpleExample()
+        {
+            var sut = new HasPathSumProblem();
+
+            var root = new TreeNode
+            {
+                val = 1,
+                left = new TreeNode
+                {
+                    val = 2
+                },
+            };
+
+            Assert.IsTrue(sut.HasPathSum(root, 3));
+        }
+
+        [Test]
+        public void HasPathSumNullExample()
+        {
+            var sut = new HasPathSumProblem();
+
+            Assert.IsFalse(sut.HasPathSum(null, 0));
+        }
+    }
+}
