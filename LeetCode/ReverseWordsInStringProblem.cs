003b3ef4ed57447462bd367b5024fac18425e20f
Peter Tabatt <ptabatt@gmail.com>
2023-11-06 12:42:33 -0700

    ReverseString problem

---
+++ LeetCode/ReverseWordsInStringProblem.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+    public class ReverseWordsInStringProblem
+    {
+        public string ReverseWords(string s)
+        {
+            var words = s.Split(' ');
+            var sb = new System.Text.StringBuilder();
+
+            for (var i = words.Length - 1; i >= 0; i--)
+            {
+                if (!string.IsNullOrWhiteSpace(words[i]))
+                {
+                    sb.Append(words[i] + " ");
+                }
+            }
+
+            return sb.ToString().Trim(' ');
+        }
+    }
+}
---
+++ LeetCodeUnitTest/ReverseWordsInStringTest.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class ReverseWordsInStringTest
+    {
+        [TestCase("the sky is blue", "blue is sky the")]
+        [TestCase("a good   example", "example good a")]
+        [TestCase("  hello world  ", "world hello")]
+        public void ReverseWordsInStringTests(string given, string expected)
+        {
+            var sut = new ReverseWordsInStringProblem();
+
+            Assert.AreEqual(expected, sut.ReverseWords(given));
+        }
+    }
+}
