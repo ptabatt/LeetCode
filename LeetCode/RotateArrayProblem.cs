fb4e5ba164424c555b04e3ccf5953a4aa1f4b759
Peter Tabatt <ptabatt@gmail.com>
2023-11-06 11:47:49 -0700

    RotateArrayProblem

---
+++ LeetCode/RotateArrayProblem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class RotateArrayProblem
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            if (nums.Length < 2 || k < 1)
+            {
+                return;
+            }
+
+            var n = nums.Length;
+            var tmp = new int[n];
+
+            if (k > n)
+            {
+                k %= n;
+            }
+
+            var ii = n - k;
+
+            for (var i = 0; i < n; i++)
+            {
+                if (ii > n - 1)
+                {
+                    tmp[i] = nums[ii - n];
+                }
+                else
+                {
+                    tmp[i] = nums[ii];
+                }
+
+                ii++;
+            }
+
+            for (var i = 0; i < tmp.Length; i++)
+            {
+                nums[i] = tmp[i];
+            }
+        }
+    }
+}
---
+++ LeetCodeUnitTest/RotateArrayTest.cs
@@ -0,0 +1,69 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class RotateArrayTest
+    {
+        [Test]
+        public void RotateArrayExample1()
+        {
+            var sut = new RotateArrayProblem();
+            var given = new[] { 1, 2, 3, 4, 5, 6, 7 };
+            var expected = new[] { 5, 6, 7, 1, 2, 3, 4 };
+
+            sut.Rotate(given, 3);
+
+            Assert.AreEqual(expected, given);
+        }
+
+        [Test]
+        public void RotateArrayExample2()
+        {
+            var sut = new RotateArrayProblem();
+            var given = new[] { -1, -100, 3, 99 };
+            var expected = new[] { 3, 99, -1, -100 };
+
+            sut.Rotate(given, 2);
+
+            Assert.AreEqual(expected, given);
+        }
+
+        [Test]
+        public void RotateArrayExample3()
+        {
+            var sut = new RotateArrayProblem();
+            var given = new[] { 1, 2, 3 };
+            var expected = new[] { 3, 1, 2 };
+
+            sut.Rotate(given, 1);
+
+            Assert.AreEqual(expected, given);
+        }
+
+        [Test]
+        public void RotateArrayEdgeCase1()
+        {
+            var sut = new RotateArrayProblem();
+            var given = new[] { 1, 2 };
+            var expected = new[] { 2, 1 };
+
+            sut.Rotate(given, 3);
+
+            Assert.AreEqual(expected, given);
+        }
+
+        [Test]
+        public void RotateArrayEdgeCase2()
+        {
+            var sut = new RotateArrayProblem();
+            var given = new[] { 1, 2, 3 };
+            var expected = new[] { 3, 1, 2 };
+
+            sut.Rotate(given, 4);
+
+            Assert.AreEqual(expected, given);
+        }
+    }
+}
