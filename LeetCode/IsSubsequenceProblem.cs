aa51ff6f59cef098117bb2cc961908ba053e6d5c
Peter Tabatt <ptabatt@gmail.com>
2023-11-08 15:20:13 -0700

    Is subsequence problem

---
+++ LeetCode/IsSubsequenceProblem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LeetCode
+{
+    // https://leetcode.com/problems/is-subsequence/?envType=study-plan-v2&envId=top-interview-150
+    public class IsSubsequenceProblem
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return true;
+            }
+
+            if (string.IsNullOrEmpty(s) && string.IsNullOrEmpty(t))
+            {
+                // weird edge case provided by Leetcode. I would argue
+                // that nothing can be a substring of an empty string
+                return true;
+            }
+
+            var j = 0;
+
+            for (var i = 0; i < t.Length; i++)
+            {
+                if (s[j] == t[i])
+                {
+                    j++;
+                }
+
+                if (j > s.Length - 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/IsSubsequenceTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class IsSubsequenceTest
+    {
+        [TestCase("abc", "ahbgdc", true)]
+        [TestCase("axc", "ahbgdc", false)]
+        [TestCase("", "ahbgdc", true)]
+        [TestCase("", "", true)]
+        [TestCase("b", "c", false)]
+        public void IsSubsequenceLeetCodeExamples(string s, string t, bool expected)
+        {
+            var sut = new IsSubsequenceProblem();
+            Assert.AreEqual(expected, sut.IsSubsequence(s, t));
+        }
+    }
+}
