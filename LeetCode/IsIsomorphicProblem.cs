d1efeeaa5e069e352e3b1200b24a771fece026b8
Peter Tabatt <ptabatt@gmail.com>
2023-12-06 11:09:56 -0700

    IsIsomorphicProblem

---
+++ LeetCode/IsIsomorphicProblem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class IsIsomorphicProblem
+    {
+        // https://leetcode.com/problems/isomorphic-strings/
+        // Constraints:  
+
+        // 1 <= s.length <= 5 * 104
+        // t.length == s.length
+        // s and t consist of any valid ascii character.
+        public bool IsIsomorphic(string s, string t)
+        {
+            var dct = new Dictionary<char, char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dct.TryGetValue(s[i], out char val))
+                {
+                    if (!(t[i] == val))
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (dct.ContainsValue(t[i]))
+                    {
+                        return false;
+                    }
+
+                    dct[s[i]] = t[i];
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/IsIsomorphicTest.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class IsIsomorphicTest
+    {
+        [TestCase("egg","add", true)]
+        [TestCase("foo", "bar", false)]
+        [TestCase("paper", "title", true)]
+        [TestCase("badc", "baba", false)]
+        public void IsIsomorphicBasicTest(string givenS, string givenT, bool expected) 
+        {
+            var sut = new IsIsomorphicProblem();
+            Assert.That(sut.IsIsomorphic(givenS, givenT), Is.EqualTo(expected));
+        }
+    }
+}
