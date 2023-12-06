9f48081d25369acf77c00719dba292238f369eee
Peter Tabatt <ptabatt@gmail.com>
2023-12-06 14:56:51 -0700

    Jump Game Problem.... oh dear...

---
+++ LeetCode/JumpGameProblem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class JumpGameProblem
+    {
+        // https://leetcode.com/problems/jump-game/?envType=study-plan-v2&envId=top-interview-150
+        public bool CanJump(int[] nums)
+        {
+            var dct = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length - 1; i++) 
+            {
+                if (nums[i] == 0)
+                {
+                    return false;
+                }
+
+                if (i + nums[i] >= nums.Length - 1)
+                {
+                    return true;
+                }
+
+                foreach (int j in Enumerable.Range(1, nums[i]))
+                {
+                    dct.Add(i + j, nums[i + j] + j);
+                }
+
+                var max = dct.Aggregate((l, r) => l.Value > r.Value ? l : r);
+                dct.Clear();
+
+                i = max.Key - 1;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/JumpGameTest.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class JumpGameTest
+    {
+        [TestCase(new[] { 2, 3, 1, 1, 4 }, true)]
+        [TestCase(new[] { 3, 2, 1, 0, 4 }, false)]
+        [TestCase(new[] { 1, 1, 1, 1, 4 }, true)]
+        [TestCase(new[] { 8, 0, 0, 0, 0, 0, 0, 0, 0 }, true )]
+        [TestCase(new[] { 8, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, false)]
+        [TestCase(new[] { 0 }, true)]
+        [TestCase(new[] { 10, 11 }, true)]
+        [TestCase(new[] { 5, 1, 1, 2, 3, 4, 0, 0, 0, 11 }, true)]
+        [TestCase(new[] { 0, 1 }, false)]
+        [TestCase(new[] { 4, 2, 0, 0, 1, 1, 4, 4, 4, 0, 4, 0 }, true)]
+        public void JumpGameTestCases(int[] given, bool expected)
+        {
+            var sut = new JumpGameProblem();
+            Assert.That(sut.CanJump(given), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void JumpGameStressTest()
+        {
+            var arr = new int[1000];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = 100000;
+            }
+
+            var sut = new JumpGameProblem();
+            Assert.That(sut.CanJump(arr), Is.True);
+        }
+    }
+}
