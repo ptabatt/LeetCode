0a0d7406ef47c113cb6f1ea8c1e4a74846188b54
Peter Tabatt <ptabatt@gmail.com>
2024-01-09 14:56:43 -0700

    Word Pattern problem

---
+++ LeetCode/WordPatternProblem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class WordPatternProblem
+    {
+        // https://leetcode.com/problems/word-pattern/?envType=study-plan-v2&envId=top-interview-150
+        public bool WordPattern(string pattern, string s)
+        {
+            var dct = new Dictionary<char, string>();
+            var arr = s.Split(' ');
+
+            if (pattern.Length != arr.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (dct.ContainsKey(pattern[i]))
+                {
+                    if (!string.Equals(dct[pattern[i]], arr[i]))
+                    {
+                        return false;
+                    }
+                } 
+                else if (dct.ContainsValue(arr[i]))
+                {
+                    return false;
+                }
+                else
+                {
+                    dct[pattern[i]] = arr[i];
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/WordPatternTest.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class WordPatternTest
+    {
+        [TestCase("abba", "dog cat cat dog", true)]
+        [TestCase("abba", "dog cat cat fish", false)]
+        [TestCase("aaaa", "dog cat cat dog", false)]
+        [TestCase("a", "word test", false)]
+        [TestCase("zzz", "one", false)]
+        [TestCase("abcdefg", "one two three four five six seven", true)]
+        public void WordPatternLeetCodeExamples(string pattern, string s, bool expected)
+        {
+            var objSut = new WordPatternProblem();
+
+            Assert.That(objSut.WordPattern(pattern, s), Is.EqualTo(expected));
+        }
+    }
+}
