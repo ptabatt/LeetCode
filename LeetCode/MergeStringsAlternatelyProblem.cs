d7cc44836b4753f41308dab7a3a4ebe5f5d3eff0
Peter Tabatt <ptabatt@gmail.com>
2024-01-17 20:21:44 -0700

    MergeAlternately problem

---
+++ LeetCode/MergeStringsAlternatelyProblem.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace LeetCode
+{
+    public class MergeStringsAlternatelyProblem
+    {
+        // https://leetcode.com/problems/merge-strings-alternately/
+        public string MergeAlternately(string word1, string word2)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < word1.Length + word2.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    int idx = i / 2;
+                    
+                    if (idx < word1.Length)
+                    {
+                        sb.Append(word1[idx]);
+                    }
+                    else
+                    {
+                        while (idx < word2.Length)
+                        {
+                            sb.Append(word2[idx]);
+                            idx++;
+                        }
+
+                        break;
+                    }
+                }
+                else
+                {
+                    int idx = (i - 1) / 2;
+
+                    if (idx < word2.Length)
+                    {
+                        sb.Append(word2[idx]);
+                    }
+                    else
+                    {
+                        while (idx < word1.Length - 1)
+                        {
+                            idx++;
+                            sb.Append(word1[idx]);
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ LeetCodeUnitTest/MergeStringsAlternatelyTest.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class MergeStringsAlternatelyTest
+    {
+        [TestCase("abc", "pqr", "apbqcr")]
+        [TestCase("ab", "pqrs", "apbqrs")]
+        [TestCase("abcd", "pq", "apbqcd")]
+        public void MergeStringsLeetCodeCases(string word1, string word2, string expected)
+        {
+            var sut = new MergeStringsAlternatelyProblem();
+            Assert.That(sut.MergeAlternately(word1, word2), Is.EqualTo(expected));
+        }
+    }
+}
