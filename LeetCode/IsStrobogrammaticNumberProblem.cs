923a1957fbe732edc7aab8fe1dfa21955f6980e9
Peter Tabatt <ptabatt@gmail.com>
2024-01-18 14:09:41 -0700

    Is Strobogrammatic number

---
+++ LeetCode/IsStrobogrammaticNumberProblem.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class IsStrobogrammaticProblem
+    {
+        //https://leetcode.com/problems/strobogrammatic-number/
+        public bool IsStrobogrammatic(string num)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var c in num)
+            {
+                switch (c)
+                {
+                    case '0':
+                    case '1':
+                    case '8':
+                        sb.Append(c);
+                        break;
+                    case '6':
+                        sb.Append('9');
+                        break;
+                    case '9':
+                        sb.Append('6');
+                        break;
+                    default:
+                        return false;
+                }
+            }
+
+            var reversed = new string(sb.ToString().Reverse().ToArray());
+            return Equals(reversed, num);
+        }
+    }
+}
---
+++ LeetCodeUnitTest/IsStrobogrammaticNumberTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class IsStrobogrammaticNumberTest
+    {
+        [TestCase("69", true)]
+        [TestCase("88", true)]
+        [TestCase("808", true)]
+        [TestCase("962", false)]
+        [TestCase("8888885", false)]
+        public void IsStrobogrammaticTestCases(string num, bool expected)
+        {
+            var sut = new IsStrobogrammaticProblem();
+            Assert.That(sut.IsStrobogrammatic(num), Is.EqualTo(expected));
+        }
+    }
+}
